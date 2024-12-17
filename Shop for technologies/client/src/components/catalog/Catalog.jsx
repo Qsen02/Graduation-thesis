c119226c0eb61505a5fcd3429ad94608ccc60320
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2024-12-17 12:32:35 +0200

    Implemnted styling on catalog

--- Shop for technologies/client/src/components/catalog/Catalog.jsx
+++ Shop for technologies/client/src/components/catalog/Catalog.jsx
@@ -1,26 +1,30 @@
 import { useGetAllProducts } from "../../hooks/useProducts";
 import HomeProducts from "../home/home-products/HomeProducts";
+import styles from "./Catalog.module.css";
 
 export default function Catalog() {
     const { products, isLoading, isError } = useGetAllProducts([]);
 
     return (
-        <section>
+        <section className={styles.catalogWrapper}>
             <form>
                 <h2>Търсете продукти</h2>
-                <input
-                    type="text"
-                    name="query"
-                    id="query"
-                    placeholder="Въведете критерии..."
-                />
-                <select>
-                    <option value="name">Име</option>
-                    <option value="price">Цена</option>
-                    <option value="category">Категория</option>
-                </select>
-                <button type="submit">Търси</button>
+                <div>
+                    <input
+                        type="text"
+                        name="query"
+                        id="query"
+                        placeholder="Въведете критерии..."
+                    />
+                    <select>
+                        <option value="name">Име</option>
+                        <option value="price">Цена</option>
+                        <option value="category">Категория</option>
+                    </select>
+                    <button type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
+                </div>
             </form>
+            <h2>Всички продукти</h2>
             <section>
                 {isLoading && !isError ? (
                     <span className="loader"></span>
