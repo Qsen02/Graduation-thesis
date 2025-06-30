b1989bb5380b0bad3c5b8a9049c52da2e0d8f7ce
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-06-30 14:56:21 +0300

    Environments update

--- Shop for technologies/server/src/config/mongoose.js
+++ Shop for technologies/server/src/config/mongoose.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const User = require("../models/user.js");
 const Products = require("../models/Product.js");
 const Orders = require("../models/Order.js");
+const dotenv=require("dotenv");
+dotenv.config();
 
 const localDB = "mongodb://127.0.0.1:27017/Shop-for-technologies";
 
