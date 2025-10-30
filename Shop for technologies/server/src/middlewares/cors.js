1fbf6b4c6347fbee62361e3aa0df6c9503bcd434
Ясен Генчев <101555544+Qsen02@users.noreply.github.com>
2025-10-30 12:14:40 +0200

    CORS update

--- Shop for technologies/server/src/middlewares/cors.js
+++ Shop for technologies/server/src/middlewares/cors.js
@@ -1,6 +1,10 @@
 function setCors() {
     return function(req, res, next) {
-        res.setHeader("Access-Control-Allow-Origin", "*");
+        const allowedOrigins = ["http://localhost:3000", "https://graduation-thesis-0it6.onrender.com"];
+        const origin = req.headers.origin;
+        if (allowedOrigins.includes(origin)) {
+            res.setHeader("Access-Control-Allow-Origin", origin);
+        }
         res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, DELETE, POST");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Authorization");
         next();
